2613d4de4a16be6b803067f29ef4c7a9f94ffd7c
KayandTheBlack <victor.gimenez.abalos@gmail.com>
2016-06-29 13:47:25 +0200

    Updated with Game management logic

--- src/Game.js
+++ src/Game.js
@@ -7,13 +7,25 @@ class Game {
     this.sockets = []
     this.turn = new Turn([], [], [])
   }
-  onPlayerConnected (socket) {
-    let nplayer = this.turn.bikes.length
+  onPlayerJoin (socket) {
+    var nplayer = 0
+    while (this.sockets[nplayer] !== null & nplayer < this.sockets.length) nplayer++
+    if (nplayer === this.sockets.length) this.sockets.push(socket)
+    else this.sockets[nplayer] = socket
     this.players[socket.id] = nplayer
-    this.sockets.push(socket)
     this.turn.bikes.push({ i: 0, j: 0, dir: C.UP, alive: true })
     this.turn.inputs.push(null)
-    this.turn = new Turn([], this.turn.bikes, this.turn.inputs)
+    for (let i = 0; i < this.sockets.length; i++) {
+      if (this.sockets[i] !== null) this.sockets[i].emit('game:state', {turn: this.turn, players: this.players})
+    }
+  }
+  onPlayerLeave (socket) {
+    var nplayer = this.players[socket.id]
+    this.sockets[nplayer] = null
+    delete this.players[socket.id]
+  }
+  onChangeDir (socket, dir) {
+    this.turn.setInput(this.players[socket.id], dir)
     for (let i = 0; i < this.sockets.length; i++) {
       this.sockets[i].emit('game:state', {turn: this.turn, players: this.players})
     }
